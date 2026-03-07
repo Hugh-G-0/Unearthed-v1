ac1dea1b004a53c9c489ba98e2079268c7e9c992
Hugh-G-0 <hughgranger192@gmail.com>
2026-03-07 15:10:42 -0500

    fix units

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,8 +5,6 @@ import edu.wpi.first.math.util.Units;
 public class Constants {
     
     public static final double
-    
-
         kWheelBaseX = Units.inchesToMeters(25),
         kWheelBaseY = Units.inchesToMeters(24);
     ;
